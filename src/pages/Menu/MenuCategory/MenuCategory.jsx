5580477ab6735460d0feea4838e913d884a6c873
niloysaha06 <niloysaha895@gmail.com>
2023-08-23 22:47:01 +0600

    get menu and show

--- src/pages/Menu/MenuCategory/MenuCategory.jsx
+++ src/pages/Menu/MenuCategory/MenuCategory.jsx
@@ -1,5 +1,17 @@
-const MenuCategory = () => {
-  return <div>MenuCategory</div>;
+import Cover from "../../Shared/Cover";
+import MenuItem from "../../Shared/MenuItem";
+
+const MenuCategory = ({ items, title, img }) => {
+  return (
+    <div className="pt-8">
+      {title && <Cover img={img} title={title}></Cover>}
+      <div className="grid md:grid-cols-2 gap-10 my-16">
+        {items.map((item) => (
+          <MenuItem key={item._id} item={item}></MenuItem>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default MenuCategory;

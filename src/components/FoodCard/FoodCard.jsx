f204338b2b7d9ebba12ea2245562560d2a095453
niloysaha06 <niloysaha895@gmail.com>
2023-08-25 18:16:24 +0600

    Create login form

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const FoodCard = ({ item }) => {
   const { name, image, price, recipe } = item;
 
@@ -9,11 +11,15 @@ const FoodCard = ({ item }) => {
       <p className="absolute right-0 mr-4 mt-4 px-4 bg-slate-900 text-white">
         {price}
       </p>
-      <div className="card-body text-center">
+      <div className="card-body flex flex-col items-center">
         <h2 className="card-title">{name}</h2>
         <p>{recipe}</p>
         <div className="card-actions justify-end">
-          <button className="btn btn-primary">Add to Cart</button>
+          <Link>
+            <button className="btn btn-outline bg-slate-100 border-0 border-orange-400 border-b-4 mt-3">
+              Order Now
+            </button>
+          </Link>
         </div>
       </div>
     </div>
--- src/pages/Menu/MenuCategory/MenuCategory.jsx
+++ src/pages/Menu/MenuCategory/MenuCategory.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Cover from "../../Shared/Cover";
 import MenuItem from "../../Shared/MenuItem";
 
@@ -10,6 +11,11 @@ const MenuCategory = ({ items, title, img }) => {
           <MenuItem key={item._id} item={item}></MenuItem>
         ))}
       </div>
+      <Link to={`/order/${title}`}>
+        <button className="btn btn-outline border-0 border-b-4 mt-3">
+          Order Now
+        </button>
+      </Link>
     </div>
   );
 };
